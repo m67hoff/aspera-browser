adf83f26aa5b8c6225664adfacbd26d196780ce4
m67hoff <mhoff@web.de>
2018-01-12 20:02:14 +0100

    cors serverconfig

--- node.js-server/server.js
+++ node.js-server/server.js
@@ -16,10 +16,10 @@ const LOGOUTPUT = process.stdout
 function json2s(obj) { return JSON.stringify(obj, null, 2); }  // format JSON payload for log
 
 // read in the config file and set log.level
-function setConf () {
+function setConf() {
   var c = JSON.parse(fs.readFileSync(CONFIG))
   log.level = c.LOGLEVEL
-  log.warn('log  ', 'Read Config & update \nSet LOGLEVEL to %j', c.LOGLEVEL)
+  log.warn('log  ', 'Read Config - Set LOGLEVEL to %j', c.LOGLEVEL)
   log.verbose('conf ', json2s(c))
   return c
 }
@@ -33,25 +33,27 @@ app.use(bodyParser.json());
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, appEnv.bind, function() {
-  log.info('express', 'server starting on ' + appEnv.url);
+  log.http('express', 'server starting on ' + appEnv.url);
 });
 
 // serve static files / angular web client
 app.use(express.static(__dirname + '/webclient'));
 
 // enable CORS with preflight
-const origin = 'http://localhost:4200';
-app.options('*', (req, res) => {
-  log.verbose('express', 'preflight request from: ' + req.headers.origin);
-  res.set({
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Origin': origin,
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Authorization, Content-Type, nodeURL'
+if (config.ENABLE_CORS) {
+  log.http('cors', 'set CORS origin to ' + config.CORS_ORIGIN);
+  app.options('*', (req, res) => {
+    log.http('express', 'preflight request from: ' + req.headers.origin);
+    res.set({
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Origin': config.CORS_ORIGIN,
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Authorization, Content-Type, nodeURL'
+    });
+    res.sendStatus(200);
+    log.verbose('express', 'preflight response:\n', json2s(res._headers));
   });
-  res.sendStatus(200);
-  log.verbose('express', 'preflight response:\n', json2s(res._headers));
-});
+}
 
 // listen on supported nodeAPI REST endpoints
 app.get('/info', makeNodeRequest);
@@ -63,19 +65,21 @@ app.post('/files/create', makeNodeRequest);
 
 function makeNodeRequest(localReq, localRes) {
   const options = {};
-  options.url = localReq.headers.nodeurl + localReq.path;
-  log.info('makeNodeRequest', options.url);
+  options.url = ( config.FIX_NODEAPI_URL !== '') ? config.FIX_NODEAPI_URL : localReq.headers.nodeurl;
+  options.url += localReq.path;
+  log.http('makeNodeRequest', options.url);
   options.method = localReq.method;
   options.json = localReq.body;
   options.headers = localReq.headers;
   log.verbose('makeNodeRequest', 'options:\n', json2s(options));
 
   nodeRequest(options, (error, remoteRes, remoteBody) => {
-    localRes.set({
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Origin': origin
-    });
-
+    if (config.ENABLE_CORS) {
+      localRes.set({
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': config.CORS_ORIGIN
+      });
+    }
     if (error) {
       log.error('remoteNodeRequest', 'error:', error);
       switch (error.code) {
@@ -93,7 +97,7 @@ function makeNodeRequest(localReq, localRes) {
       }
       localRes.json(error);
     } else {
-      log.info('remoteNodeRequest', 'statusCode:', remoteRes.statusCode);
+      log.http('remoteNodeRequest', 'statusCode:', remoteRes.statusCode);
       log.verbose('remoteNodeRequest', 'body:\n', json2s(remoteBody));
       localRes.status(remoteRes.statusCode)
         .json(remoteBody);
