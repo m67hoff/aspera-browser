b4439513c81b6b038fec869857b4e041615f3c76
m67hoff <mhoff@web.de>
2018-08-25 18:10:27 +0200

    rate pipe + 3 digit out for size

--- src/app/pipes/size.pipe.ts
+++ src/app/pipes/size.pipe.ts
@@ -1,18 +1,39 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'size'
-})
+@Pipe({ name: 'size' })
 export class SizePipe implements PipeTransform {
 
   transform(bytes: number): string {
     const kB = 1024;
-    const p = 0;
-    if (bytes > Math.pow(kB, 4)) { return (bytes / Math.pow(kB, 4)).toFixed(p) + ' TB'; }
-    if (bytes > Math.pow(kB, 3)) { return (bytes / Math.pow(kB, 3)).toFixed(p) + ' GB'; }
-    if (bytes > Math.pow(kB, 2)) { return (bytes / Math.pow(kB, 2)).toFixed(p) + ' MB'; }
-    if (bytes > kB) { return (bytes / kB).toFixed(p) + ' kB'; }
+    const p = 1;
+
+    if (bytes >= Math.pow(kB, 4)) { return (bytes / Math.pow(kB, 4)).toFixed(p) + ' TB'; }
+    if (bytes >= 100 * Math.pow(kB, 3)) { return (bytes / Math.pow(kB, 3)).toFixed(0) + ' GB'; }
+    if (bytes >= 10 * Math.pow(kB, 3)) { return (bytes / Math.pow(kB, 3)).toFixed(1) + ' GB'; }
+    if (bytes >= Math.pow(kB, 3)) { return (bytes / Math.pow(kB, 3)).toFixed(2) + ' GB'; }
+    if (bytes >= 100 * Math.pow(kB, 2)) { return (bytes / Math.pow(kB, 2)).toFixed(0) + ' MB'; }
+    if (bytes >= 10 * Math.pow(kB, 2)) { return (bytes / Math.pow(kB, 2)).toFixed(1) + ' MB'; }
+    if (bytes >= Math.pow(kB, 2)) { return (bytes / Math.pow(kB, 2)).toFixed(2) + ' MB'; }
+    if (bytes >= 100 * kB) { return (bytes / kB).toFixed(0) + ' kB'; }
+    if (bytes >= 10 * kB) { return (bytes / kB).toFixed(1) + ' kB'; }
+    if (bytes >= kB) { return (bytes / kB).toFixed(2) + ' kB'; }
+ 
     return bytes + ' B';
+  }
+
+}
+
+@Pipe({ name: 'rate' })
+export class RatePipe implements PipeTransform {
+
+  transform(kbps: number): string {
+    const kB = 1024;
+    const p = 1;
+
+    if (kbps > Math.pow(kB, 2)) { return (kbps / Math.pow(kB, 2)).toFixed(p) + ' Gbps'; }
+    if (kbps > kB) { return (kbps / kB).toFixed(p) + ' Mbps'; }
+
+    return kbps + ' kbps';
   }
 
 }
