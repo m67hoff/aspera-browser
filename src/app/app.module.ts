49b496b66579831b395d193bea38ae8f02d25daa
m67hoff <mhoff@web.de>
2018-08-30 09:58:21 +0200

    hammer.js to polyfills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import 'hammerjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialImportModule } from './material-import.module';
