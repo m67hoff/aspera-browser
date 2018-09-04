e86000d11b2f334a850f98311642d86879e3b2a0
m67hoff <mhoff@web.de>
2018-09-04 18:45:31 +0200

    lint

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -12,7 +12,7 @@ export class InfoComponent {
     {url: 'https://demo.asperasoft.com', name: 'Aspera Demoserver', detail: 'official Site for the Asper Demoserver used in this example'},
     {url: 'https://github.com/m67hoff/aspera-browser', name: 'AsperaBrowser @ GitHub', detail: 'Source and Docu for this Application (for Developers & Admins)'},
     {url: 'https://www.rubydoc.info/gems/asperalm', name: 'Laurents Aspera Command Line Interface', detail: 'aslmcli - command line interface to Aspera Applications and Rest API'}
-  ]
+  ];
 
   constructor() { }
 
